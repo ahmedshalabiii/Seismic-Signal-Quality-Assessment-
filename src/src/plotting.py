ff1b1c8323e77d55d822599b28e3f3a059867f77
ahmedshalabiii <ahmed.mu.shalaby@gmail.com>
2025-06-24 21:42:25 +0300

    Create plotting.py

---
+++ src/src/plotting.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import os
+
+def plot_loss_curves(train_losses, val_losses, save_path=None):
+    plt.figure(figsize=(8, 5))
+    plt.plot(train_losses, label='Training Loss', marker='o')
+    plt.plot(val_losses, label='Validation Loss', marker='o')
+    plt.title('Loss Curves')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    if save_path:
+        plt.savefig(os.path.join(save_path, "loss_curve.png"))
+    else:
+        plt.show()
+    plt.close()
+
+
+def plot_accuracy_curves(train_acc, val_acc, save_path=None):
+    plt.figure(figsize=(8, 5))
+    plt.plot(train_acc, label='Training Accuracy', marker='o')
+    plt.plot(val_acc, label='Validation Accuracy', marker='o')
+    plt.title('Accuracy Curves')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid(True)
+    if save_path:
+        plt.savefig(os.path.join(save_path, "accuracy_curve.png"))
+    else:
+        plt.show()
+    plt.close()
+
